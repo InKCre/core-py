2ed20981de30c9740bdf3f1456f8d907b5d3b70d
Lan_zhijiang <lanzhijiang@foxmail.com>
2025-09-17 15:01:14 +0800

    ref db | 合并各环境变量为 CONN_STRING

--- app/engine.py
+++ app/engine.py
@@ -1,34 +1,25 @@
 __all__ = [
-    'SQLDB_ENGINE',
-    'get_db_session',
-    'SessionLocal',
+    "SQLDB_ENGINE",
+    "get_db_session",
+    "SessionLocal",
 ]
 
 import os
 import typing
-import sqlalchemy.orm
-import sqlalchemy.dialects.postgresql
-# from sqlalchemy import create_engine
 import sqlmodel
 
 
 # configs
-USERNAME = os.getenv('DB_USERNAME', 'root')
-PASSWORD = os.getenv('DB_PASSWORD', '')
-HOST = os.getenv('DB_HOST', 'localhost')
-PORT = int(os.getenv('DB_PORT', '5432'))
-DATABASE = os.getenv('DB_DATABASE', 'public')
-SCHEMA = os.getenv('DB_SCHEMA', 'public')
+DB_CONN_STRING = os.getenv("DB_CONN_STRING", "")
 
 
-SQLDB_ENGINE = sqlmodel.create_engine(
-    f'postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}',
-    connect_args={"options": f"-csearch_path={SCHEMA}"}
-)
+SQLDB_ENGINE = sqlmodel.create_engine(url=DB_CONN_STRING, pool_pre_ping=True)
+
 
 # SessionLocal = sqlalchemy.orm.sessionmaker(autocommit=False, autoflush=False, bind=SQLDB_ENGINE)
 def SessionLocal():
     return sqlmodel.Session(SQLDB_ENGINE)
+
 
 def get_db_session() -> typing.Generator:
     """A fastapi dependency to get a database session."""
